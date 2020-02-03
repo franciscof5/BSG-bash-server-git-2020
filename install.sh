96fb55fec81a0a2fb983fa826d2bfd6926bbadc6
Francisco Mat <francisco.matulovic@ftd.com.br>
2020-02-03 11:00:52 -0300

    Install mld

--- install.sh
+++ install.sh
@@ -7,15 +7,22 @@ rm -rf /bin/bsg
 rm -rf /bin/bsg.conf
 cp bsg.sh /bin/bsg
 cp bsg.conf /bin/bsg.conf
+sudo chmod +x /bin/bsg
 
 #SCRIPTS
 rm -rf /bin/gac
 cp bsg-scripts/gac /bin/gac
+sudo chmod +x /bin/gac
 
 rm -rf /bin/gp
 cp bsg-scripts/gp /bin/gp
+sudo chmod +x /bin/gp
+
+rm -rf /bin/mld
+cp bsg-scripts/mld /bin/mld
+sudo chmod +x /bin/mld
 
 #echo "(cd ~/bin/wpsql-cli-folder/ && exec ~/bin/wpsql-cli-folder/wpsql-cli.sh \$1 \$2 \$3)" > ~/bin/wpsql
 #ln -s ~/bin/wpsql-cli-folder/wpsql-cli.sh ~/bin/wpsql-cli
-sudo chmod +x /bin/bsg
+
 echo "Installation successfully. Now you can run 'bsg' command from any directory"
