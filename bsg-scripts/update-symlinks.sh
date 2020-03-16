a9512a45d15c4419ddb645584401d6d32d4bbdb8
Francisco Mat <francisco@f5sites.com>
2020-03-15 23:02:57 -0300

    update symlink working

--- bsg-scripts/update-symlinks.sh
+++ bsg-scripts/update-symlinks.sh
@@ -14,7 +14,7 @@ else
 fi
 
 PLUGINS_DIR="$LOCAL_SERVER_ROOT/wp-content/mu-plugins-all-domains"
-OLD_TARGET="/var/www/composer/vendor/wp/wp-content/plugins/*"
+OLD_TARGET="/var/www/composer/vendor/wp/wp-content/plugins"
 NEW_TARGET="$LOCAL_SERVER_ROOT/wp-content/plugins"
 
 #find $SRC_DIR -type l \
@@ -22,5 +22,24 @@ NEW_TARGET="$LOCAL_SERVER_ROOT/wp-content/plugins"
 #  'ln -nsf "$(readlink "%p"|sed $SUB)" "$(echo "%p"|sed $SUB)"\n'\
 # > script.sh
 
-find $PLUGINS_DIR -lname "$OLD_TARGET" \
-    -exec  sh -c 'ln -sf `echo $(readlink $0) | sed $NEW_TARGET` $0' {} \;
+#cd $PLUGINS_DIR
+#find $NEW_TARGET -lname "$OLD_TARGET" \
+#    -exec  sh -c 'ln -sf `echo $(readlink $0) | sed $NEW_TARGET` $0' {} \;
+
+DIR=$PLUGINS_DIR
+OLD_PATTERN=$OLD_TARGET
+NEW_PATTERN=$NEW_TARGET
+
+while read -r line
+do
+    echo $line
+    CUR_LINK_PATH="$(readlink "$line")"
+    echo "current: $CUR_LINK_PATH"
+    NEW_LINK_PATH="$CUR_LINK_PATH"  
+    echo "novo: $NEW_LINK_PATH"
+    NEW_LINK_PATH="${NEW_LINK_PATH/"$OLD_PATTERN"/"$NEW_PATTERN"}"
+    echo "novo fim: $NEW_LINK_PATH"
+    rm "$line"
+    ln -s "$NEW_LINK_PATH" "$line"
+done <<< $(find "$DIR" -type l)
+
