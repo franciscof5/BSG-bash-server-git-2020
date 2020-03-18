bc678086c9ea6519ea2b838f7a826cbfcbc69a08
Francisco Mat <francisco@f5sites.com>
2020-03-18 10:55:12 -0300

    create and replace /etc/hosts

--- bsg-scripts/create-etchosts.sh
+++ bsg-scripts/create-etchosts.sh
@@ -12,14 +12,29 @@ else
 	echo "You must create a $conffile file before install, please check README"
 fi
 
-echo "127.0.0.1       localhost
+hosts="127.0.0.1       localhost
 127.0.1.1       francisco-270E5J-2570EJ
-0.0.0.0         treinamentoemfoco.com.br
-0.0.0.0         cursowp.com.br
 
+#AUTOMATICALLY CREATED BY BSG
+"
+for i in "${domains[@]}"
+do
+	:	
+	hosts="${hosts}$LOCAL_IP		$i
+"
+done
+
+hosts="${hosts}
 # The following lines are desirable for IPv6 capable hosts
 ::1     ip6-localhost ip6-loopback
 fe00::0 ip6-localnet
 ff00::0 ip6-mcastprefix
 ff02::1 ip6-allnodes
-ff02::2 ip6-allrouters" > sudo tee /etc/hosts
+ff02::2 ip6-allrouters"
+
+echo "${hosts}"
+sudo rm -rf /etc/hosts
+echo "${hosts}" | sudo tee -a /etc/hosts
+echo "######## CHECKING IF HOSTS ARE REPLACED BY BSG ########"
+echo "cat /etc/hosts"
+cat /etc/hosts
