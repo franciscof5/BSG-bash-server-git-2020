e330bf6c34d6297a380fa26ea719bcf6732adc5b
Francisco Mat <francisco@f5sites.com>
2020-03-23 21:11:44 -0300

    create backup etc/hosts

--- bsg-scripts/remove-bsghosts.sh
+++ bsg-scripts/remove-bsghosts.sh
@@ -4,6 +4,9 @@ echo "remove-etchosts"
 hosts=`cat /etc/hosts`
 
 hosts=${hosts%%#BSG-START*}
+echo "MAKING /etc/hosts-bkp"
+cp /etc/hosts /etc/hosts-bkp
+echo "REPLACING"
 sudo rm -rf /etc/hosts
 echo "${hosts}" | sudo tee -a /etc/hosts
 echo "######## CHECKING IF HOSTS ARE REPLACED BY BSG ########"
