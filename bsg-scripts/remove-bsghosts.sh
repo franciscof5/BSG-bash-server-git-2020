9d975cd2e4182dd8a77c4c7429dab5e96d6c7f7f
Francisco Mat <francisco@f5sites.com>
2020-03-23 21:14:33 -0300

    echo better operation desc

--- bsg-scripts/remove-bsghosts.sh
+++ bsg-scripts/remove-bsghosts.sh
@@ -7,7 +7,7 @@ sudo cp /etc/hosts /etc/hosts-bkp
 hosts=`cat /etc/hosts`
 hosts=${hosts%%#BSG-START*}
 
-echo "REPLACING"
+echo "GENERATING NEW FILE /etc/hosts"
 sudo rm -rf /etc/hosts
 echo "${hosts}" | sudo tee -a /etc/hosts
 echo "######## CHECKING IF HOSTS ARE REPLACED BY BSG ########"
