0cb33ea14e62dbfc04733f55fbb658d52063a1e8
Francisco Mat <francisco@f5sites.com>
2020-05-09 06:59:05 -0300

    create vhosts on dd

--- bsg.sh
+++ bsg.sh
@@ -57,10 +57,11 @@ case "$OPERATION" in
 		sudo chmod 777 -R $LOCAL_DOCKER_FOLDER
 		echo "cd $LOCAL_DOCKER_FOLDER"
 		cd $LOCAL_DOCKER_FOLDER
+		echo "CREATE VHOSTS"
+		create-vhosts
 		echo "CLONING DOCKER: git clone --recursive -j8 $GIT_DOCKER $LOCAL_DOCKER_FOLDER"
 		git clone --recursive -j8 $GIT_DOCKER $LOCAL_DOCKER_FOLDER
 		echo "ATTEMP TO CREATE VHOSTS"		
-		create-sites-available
 		sudo service docker start
 		#docker build .
 		sudo docker-compose down
